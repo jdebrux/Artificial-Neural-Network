76abd3cf01380ead281c189f10213cb31bd9f4e8
jdebrux <joshdebruxelles@gmail.com>
2022-03-28 01:45:50 +0100

    Outputs updated for input layer.

--- new.py
+++ new.py
@@ -2,6 +2,10 @@ import numpy as np
 import csv
 from random import random
 
+"""
+The perceptron class defines the storage of data for a given perceptron in the neural network.
+The perceptrons will be used to build up each layer and thus form a Multi Layer Perceptron.
+"""
 class Perceptron:
 
     def __init__(self, inputs, nOut):
@@ -11,7 +15,9 @@ class Perceptron:
         self.output = 0.0
         self.delta = 0.0
         self.db = 0.0
-
+"""
+The MLP class represents a Multi-Layer Perceptron, and the methods required to train it.
+"""
 class MLP:
 
     def __init__(self, layers):
@@ -19,41 +25,47 @@ class MLP:
 
     def forwardProp(self):
         output = []
-        for i, layer in enumerate(self.layers):
-            new_inputs = output
+        for i, layer in enumerate(self.layers): #loop through each layer in the MLP
+            #store the inputs for a given layer as the outputs of the previous layer
+            new_inputs = output 
             output = []
-            print("I:",i)
-            if i!=0:
+            if i!=0: #check if we are at the input layer
                 #get weights of previous layer's perceptrons
                 prev_weights = [self.layers[i-1][j].weights for j in range(len(self.layers[i-1]))]
-                for j, perceptron in enumerate(layer):
-                    if new_inputs:
-                        perceptron.inputs = new_inputs
+                for j, perceptron in enumerate(layer): #loop through each perceptron in the layer
+                    if new_inputs: #prevent blank inputs
+                        perceptron.inputs = new_inputs 
                     incoming_weights = []
-                    for ar in prev_weights:
+                    #loop through each set of previous weights and find the weights correlating to the current perceptron
+                    for ar in prev_weights: 
                         incoming_weights.append(ar[j])
-                    #weights . inputs + bias 
-                    if(i==len(self.layers)-1):
-                        print("Bias:",perceptron.bias)
-                    s = sum(np.multiply(perceptron.inputs, incoming_weights)) + perceptron.bias
-                    new_activation = self.sigmoid(s)
+                    s = sum(np.multiply(perceptron.inputs, incoming_weights)) + perceptron.bias #weights . inputs + bias 
+                    new_activation = self.sigmoid(s) #apply the sigmoid function
+                    #if i==1:
+                        #print("Old perceptron:", perceptron.output)
                     perceptron.output = new_activation
+                    #if i==1:
+                        #print("New perceptron:", perceptron.output)
                     output.append(new_activation)
-                    #print("Output:",output)
+            else:
+                for j,perceptron in enumerate(layer):
+                    perceptron.output = perceptron.inputs[0]
                 
-        #print(output)
-        return output
+        return new_activation
 
     def backwardProp(self, correct):
         outputs = []
-        for i, layer in reversed(list(enumerate(self.layers))):
-            for perceptron in layer:
+        error=0.0
+        for i, layer in reversed(list(enumerate(self.layers))): #loop through each layer in the MLP in reverse
+            for perceptron in layer: #loop through each perceptron in the current layer
                 f_prime = perceptron.output*(1.0-perceptron.output) #sigmoid derivative
-                delta = (correct-perceptron.output)*f_prime
-                outputs.append(delta)
+                delta = (correct-perceptron.output)*f_prime #calculate delta
                 perceptron.delta = delta
-
-        return outputs
+                #print(i,"|",perceptron.output)
+                #print("ERROR",correct-perceptron.output)
+                if i==2:
+                    error = perceptron.output-correct
+        return error
 
     def gradientDescent(self, p):
         for i in range(len(self.layers)):
@@ -62,7 +74,6 @@ class MLP:
                 deltas = []
                 for j in range(len(self.layers[i][0].weights)):
                     deltas.append(self.layers[i+1][j].delta) #get deltas of next layer's perceptrons
-                #print("Deltas:",deltas)
             for perceptron in self.layers[i]:
                 if i<len(self.layers)-1:
                     weight_change = [x*p for x in deltas]
@@ -76,37 +87,39 @@ class MLP:
         return y
 
     def train(self, dataset, epochs, p):
-        for epoch in range(epochs):   # repeat for N epochs
-            for i, row in enumerate(dataset):  # for every row
-                self.forwardProp()
-                self.backwardProp(1)   # correct value = last column in input data
-                self.gradientDescent(p)
+        for epoch in range(epochs):   #loop for given number of epochs
+            for i, row in enumerate(dataset):  #loop through every row in the data set
+                correct = row[4] #define correct value as the value for Skelton on the proceeding day
+                prediction = self.forwardProp() #perform forward propogation of the network
+                error = self.backwardProp(correct)  #perform backward propogation on the network
+                if i==len(dataset)-1: #only output for final prediction made
+                      print("Prediction:",prediction)
+                      print("Error: ",error)
+                self.gradientDescent(p) #update weights and biases
                 if i != len(dataset)-1:
                     new_inputs = dataset[i+1]
-                # print("new inputs:", new_inputs)
-                for j, perceptron in enumerate(self.layers[0]):
+                for j, perceptron in enumerate(self.layers[0]): #define inputs for each 
                     perceptron.inputs = [new_inputs[j]]
-                    # print("neuron inputs:", neuron.inputs)
-            print("epochs:", str(epoch+1))
+            #print("Epoch:", str(epoch+1))
         
 if __name__ == "__main__":
-    file = open('test_data.csv', 'r')
+    file = open('data.csv', 'r')
     data_reader = csv.reader(file, delimiter=',')
-    dataset_str = [row[1:9] for row in data_reader]   # only number values from table
-    dataset_numstr = dataset_str[2:98]   # remove 98 when passing cleaned data
-    dataset = [[float(dataset_numstr[i][j]) for j in range(len(dataset_numstr[i]))]   # convert all str columns to float
-           for i in range(len(dataset_numstr))]
+    dataset_str = [row[0:5] for row in data_reader]   # only number values from table
+    dataset = [[float(dataset_str[i][j]) for j in range(len(dataset_str[i]))]   # convert all str columns to float
+           for i in range(len(dataset_str))]
 
     nHidden = int(input("Enter the number of hidden neurons:")) #get number of hidden nodes
     
-    input_layer = [Perceptron([column], nHidden) for column in dataset[0]] #initialise input layer
+    input_layer = [Perceptron([dataset[0][i]], nHidden) for i in range(len(dataset[0])-1)] #initialise input layer
     hidden_layer = [Perceptron([0]*len(input_layer), 1) for k in range(nHidden)] #initialise hidden layer
     output_layer = [Perceptron([0]*len(hidden_layer), 1)] #initialise output layer
     
     # create network
     mlp = MLP([input_layer, hidden_layer, output_layer])
     #train
+    print(len(mlp.layers[0]))
     mlp.train(dataset,1000,0.5)
     # make a prediction:
-    output = mlp.forwardProp()[-1]    # get output from output layer
-    print("Next predicted mean daily flow at Skelton:", Calculator.destandardise(output))
+    output = mlp.forwardProp()   # get output from output layer
+    #print("Next predicted mean daily flow at Skelton:", Calculator.destandardise(output))
